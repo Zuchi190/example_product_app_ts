f7b87160d3c40496bacd06d354c2a5d0e0675669
Genki <g.kato@anytive.com>
2022-08-07 19:00:24 +0900

    finish base dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {Header} from "./components/Header"
 import {BaseRouter} from "./router/BaseRouter"
 import { BrowserRouter } from "react-router-dom";
--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,10 +1,52 @@
 import React from "react";
-import {memo} from "react"
+import { useForm } from "react-hook-form";
 
-export const New:React.FC =memo(()=>{
-    return(
-        <div>
-            <h1>事例一覧ページです</h1>
-        </div>
-    )
-})
+
+export const New=(()=> {
+   
+  return (
+    <section className="mt-60 flex justify-center">
+        <div className="bg-red-50"></div>
+    <form className="w-full max-w-sm">
+    <div className="md:flex md:items-center mb-6">
+      <div className="md:w-1/3">
+        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
+          製品名
+        </label>
+      </div>
+      <div className="md:w-2/3">
+        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="製品名を入力してください" />
+      </div>
+    </div>
+    <div className="md:flex md:items-center mb-6">
+      <div className="md:w-1/3">
+        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" >
+          製品特徴
+        </label>
+      </div>
+      <div className="md:w-2/3">
+        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="特徴が入ります" />
+      </div>
+    </div>
+    <div className="md:flex md:items-center mb-6">
+      <div className="md:w-1/3"></div>
+      <label className="md:w-2/3 block text-gray-500 font-bold">
+        <input className="mr-2 leading-tight" type="checkbox" />
+        <span className="text-sm">
+          登録前の最終確認
+        </span>
+      </label>
+    </div>
+    <div className="md:flex md:items-center">
+      <div className="md:w-1/3"></div>
+      <div className="md:w-2/3">
+        <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        データ登録
+        </button>
+      </div>
+    </div>
+  </form>
+  </section>
+  );
+}
+)
